4ba45ed5bfdbcb7cbe6b5885d483fa2143fdfce9
Connor Coley <connor.coley@gmail.com>
2016-01-15 06:49:15 -0800

    Small changes to neural_name_to_mw using texts_to_matrix

--- makeit/main/neural_name_to_mw.py
+++ makeit/main/neural_name_to_mw.py
@@ -1,4 +1,4 @@
-from makeit.utils.chemnet_connect import * # mongodb connection, gets 'chemicals', 'reactions'
+from __future__ import print_function
 from makeit.utils.parsing import SplitChemicalName 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
@@ -17,7 +17,7 @@ def get_model_fpath():
 	fpath_root = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'models')
 	return os.path.join(fpath_root, 'neural_name_to_mw')
 
-def build_model(vocab_size):
+def build_model(vocab_size, embedding_size = 100):
 	'''Generates simple embedding model to use tokenized chemical name as
 	input in order to predict a single-valued output (i.e., MW)'''
 
@@ -25,12 +25,16 @@ def build_model(vocab_size):
 	model = Sequential()
 
 	# Add layers
-	model.add(Embedding(vocab_size, 100, 
+	model.add(Embedding(vocab_size, embedding_size, 
 		init = 'uniform', input_length = None, W_regularizer = None, 
 		activity_regularizer = None, W_constraint = None, 
 		mask_zero = False, weights = None))
+	print('    model: added Embedding layer ({} -> {})'.format(vocab_size, 
+		embedding_size))
 	model.add(Dropout(0.5))
+	print('    model: added Dropout layer')
 	model.add(LSTM(output_dim = 1))
+	print('    model: added LSTM layer ({} -> {})'.format(embedding_size, 1))
 
 	# Define optimizer
 	sgd = SGD(lr = 0.1, decay = 1e-6, momentum = 0.9)
@@ -47,15 +51,15 @@ def save_model(model, tokenizer_fpath, data_fpath):
 	# Dump data
 	with open(fpath + '.json', 'w') as structure_fpath:
 		json.dump(model.to_json(), structure_fpath)
-	print '...saved structural information'
+	print('...saved structural information')
 
 	# Dump weights
-	model.save_weights(fpath + '.h5')
-	print '...saved weights'
+	model.save_weights(fpath + '.h5', overwrite = True)
+	print('...saved weights')
 
 	# Dump image
 	plot(model, to_file = fpath + '.png')
-	print '...saved image'
+	print('...saved image')
 
 	# Write to info file
 	info_fid = open(fpath + '.info', 'w')
@@ -66,7 +70,7 @@ def save_model(model, tokenizer_fpath, data_fpath):
 	info_fid.write('- data source: {}\n'.format(data_fpath))
 	info_fid.close()
 
-	print '...saved model to {}.[json, h5, png, info]'.format(fpath)
+	print('...saved model to {}.[json, h5, png, info]'.format(fpath))
 	return True
 
 def train_model(model, tokenizer, data_fpath):
@@ -90,8 +94,10 @@ def train_model(model, tokenizer, data_fpath):
 	# Vectorize chemical names according to tokenizer
 	names_train = tokenizer.texts_to_matrix(names_train)
 	names_test  = tokenizer.texts_to_matrix(names_test)
-	print 'names_train shape: {}'.format(names_train.shape)
-	print 'names_test  shape: {}'.format(names_test.shape)
+	print('names_train shape: {}'.format(names_train.shape))
+	print('names_test  shape: {}'.format(names_test.shape))
+
+	#return (model, -1)
 
 	model.fit(names_train, mws_train, nb_epoch = 5, batch_size = 4, 
 	 	      verbose = 1, show_accuracy = True)
@@ -114,18 +120,22 @@ if __name__ == '__main__':
 		tokenizer = cPickle.load(tokenizer_fid)
 
 	# Build model
-	print '...building model'
-	model = build_model(len(tokenizer.word_counts))
-	print '...built untrained model'
+	print('...building model')
+	if tokenizer.nb_words:
+		vocab_size = min([tokenizer.nb_words, len(tokenizer.word_counts)])
+	else:
+		vocab_size = len(tokenizer.word_counts)
+	model = build_model(vocab_size)
+	print('...built untrained model')
 
-	# # Train model
-	# print '...training model'
-	# (model, score) = train_model(model, tokenizer, sys.argv[2])
-	# print '...trained model, evaluated score = {}'.format(score)
+	# Train model
+	print('...training model')
+	(model, score) = train_model(model, tokenizer, sys.argv[2])
+	print('...trained model, evaluated score = {}'.format(score))
 
 	# Save for future
-	print '...saving model'
+	print('...saving model')
 	save_model(model, sys.argv[1], sys.argv[2])
-	print '...saved model'
+	print('...saved model')
 		
 
