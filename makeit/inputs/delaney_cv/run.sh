cdd6bbe60966ff1037feb35c0a51e159294a70d0
connorcoley <connor.coley@gmail.com>
2016-02-22 05:27:00 -0800

    First 5-fold CV test

---
+++ makeit/inputs/delaney_cv/run.sh
@@ -0,0 +1,5 @@
+python makeit/main/neural_fp_delaney.py makeit/inputs/delaney_cv/fold1.cfg 
+python makeit/main/neural_fp_delaney.py makeit/inputs/delaney_cv/fold2.cfg 
+python makeit/main/neural_fp_delaney.py makeit/inputs/delaney_cv/fold3.cfg 
+python makeit/main/neural_fp_delaney.py makeit/inputs/delaney_cv/fold4.cfg 
+python makeit/main/neural_fp_delaney.py makeit/inputs/delaney_cv/fold5.cfg 
