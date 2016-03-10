5cd917e54d9f034d131964625a886ff90e4a142e
connorcoley <connor.coley@gmail.com>
2016-03-10 09:22:17 -0800

    Old input files that will no longer work

--- makeit/inputs/ames_cv/run.sh
+++
@@ -1,6 +0,0 @@
-python makeit/main/neural_fp_ames.py makeit/inputs/ames_cv/fold1.cfg 
-python makeit/main/neural_fp_ames.py makeit/inputs/ames_cv/fold2.cfg 
-python makeit/main/neural_fp_ames.py makeit/inputs/ames_cv/fold3.cfg 
-python makeit/main/neural_fp_ames.py makeit/inputs/ames_cv/fold4.cfg 
-python makeit/main/neural_fp_ames.py makeit/inputs/ames_cv/fold5.cfg 
-
