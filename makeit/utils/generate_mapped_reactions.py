528bd4b70ecabcab00a545724842561953d66298
connorcoley <connor.coley@gmail.com>
2016-03-28 10:51:30 -0700

    Working(?) reaction center extraction - 80% of true products are found using radius = 1 - Many errors due to kekulization

--- makeit/utils/generate_mapped_reactions.py
+++ makeit/utils/generate_mapped_reactions.py
@@ -12,6 +12,7 @@ import json # for dumping
 import sys  # for commanad line
 import os   # for file paths
 import re 
+import itertools
 
 def mols_from_smiles_list(all_smiles):
 	'''Given a list of smiles strings, this function creates rdkit
@@ -22,117 +23,117 @@ def mols_from_smiles_list(all_smiles):
 		mols.append(Chem.MolFromSmiles(smiles))
 	return mols
 
-def all_bonds_in_mol_list(all_smiles):
-	'''Given a list of SMILES strings, this function creates rdkit
-	molecules, calculates all bonds present for any molecule in the 
-	list, and returns a list of bond tuples, where each tuple 
-	characterizes that bond.'''
-	bonds = []
-	for smiles in all_smiles:
-		if not smiles: # empty string
-			continue
-		mol = Chem.MolFromSmiles(smiles)
-		if not mol: # couldn't parse
-			print('could not parse {}'.format(smiles))
-			continue
-		for bond in mol.GetBonds():
-			bonds.append(bond_to_label(bond))
-	return bonds
+# def all_bonds_in_mol_list(all_smiles):
+# 	'''Given a list of SMILES strings, this function creates rdkit
+# 	molecules, calculates all bonds present for any molecule in the 
+# 	list, and returns a list of bond tuples, where each tuple 
+# 	characterizes that bond.'''
+# 	bonds = []
+# 	for smiles in all_smiles:
+# 		if not smiles: # empty string
+# 			continue
+# 		mol = Chem.MolFromSmiles(smiles)
+# 		if not mol: # couldn't parse
+# 			print('could not parse {}'.format(smiles))
+# 			continue
+# 		for bond in mol.GetBonds():
+# 			bonds.append(bond_to_label(bond))
+# 	return bonds
 
-def compare_bond_lists(bonds1, bonds2):
-	'''This function takes two lists of bond labels and determines the bonds
-	that were created and destroyed to get from bonds1 to bonds2'''
-	# Look for edge cases
-	if (not bonds1) and (not bonds2):
-		return ([], [])
-	if bonds1 and (not bonds2):
-		return ([], bonds1[:])
-	if (not bonds1) and bonds2:
-		return(bonds2[:], [])
+# def compare_bond_lists(bonds1, bonds2):
+# 	'''This function takes two lists of bond labels and determines the bonds
+# 	that were created and destroyed to get from bonds1 to bonds2'''
+# 	# Look for edge cases
+# 	if (not bonds1) and (not bonds2):
+# 		return ([], [])
+# 	if bonds1 and (not bonds2):
+# 		return ([], bonds1[:])
+# 	if (not bonds1) and bonds2:
+# 		return(bonds2[:], [])
 
-	# Duplicate lists to prevent aliasing issues
-	created_bonds = bonds2[:]
-	destroyed_bonds = bonds1[:]
-	# Remove overlap
-	for bond in created_bonds[:]:
-		if bond in destroyed_bonds:
-			created_bonds.remove(bond)
-			destroyed_bonds.remove(bond)
-	for bond in destroyed_bonds[:]:
-		if bond in created_bonds:
-			created_bonds.remove(bond)
-			destroyed_bonds.remove(bond)
+# 	# Duplicate lists to prevent aliasing issues
+# 	created_bonds = bonds2[:]
+# 	destroyed_bonds = bonds1[:]
+# 	# Remove overlap
+# 	for bond in created_bonds[:]:
+# 		if bond in destroyed_bonds:
+# 			created_bonds.remove(bond)
+# 			destroyed_bonds.remove(bond)
+# 	for bond in destroyed_bonds[:]:
+# 		if bond in created_bonds:
+# 			created_bonds.remove(bond)
+# 			destroyed_bonds.remove(bond)
 
-	return (created_bonds, destroyed_bonds)
+# 	return (created_bonds, destroyed_bonds)
 
-def bond_to_label(bond):
-	'''This function takes an RDKit bond and creates a label describing
-	the most important attributes'''
-	atoms = sorted([atom_to_label(bond.GetBeginAtom()), \
-				    atom_to_label(bond.GetEndAtom())])
+# def bond_to_label(bond):
+# 	'''This function takes an RDKit bond and creates a label describing
+# 	the most important attributes'''
+# 	atoms = sorted([atom_to_label(bond.GetBeginAtom()), \
+# 				    atom_to_label(bond.GetEndAtom())])
 
-	return atoms[0] + bond.GetSmarts() + atoms[1] 
-	return [atoms[0], \
-	        atoms[1], \
-	        bond.GetSmarts()]
+# 	return atoms[0] + bond.GetSmarts() + atoms[1] 
+# 	return [atoms[0], \
+# 	        atoms[1], \
+# 	        bond.GetSmarts()]
 
-def atom_to_label(atom):
-	'''This function takes an RDKit atom and creates a label describing
-	the most important attributes'''
-	return atom.GetSmarts()
-	return '{}[{}]'.format(atom.GetAtomicNum(), \
-		                   atom.GetImplicitValence())
+# def atom_to_label(atom):
+# 	'''This function takes an RDKit atom and creates a label describing
+# 	the most important attributes'''
+# 	return atom.GetSmarts()
+# 	return '{}[{}]'.format(atom.GetAtomicNum(), \
+# 		                   atom.GetImplicitValence())
 
-def bond_comparison_to_smarts(created_bonds, destroyed_bonds, agents = None):
-	'''This function creates a simple reaction SMARTS rule based on the
-	created and destroyed bonds in the molcule'''
-	# Join components separately
+# def bond_comparison_to_smarts(created_bonds, destroyed_bonds, agents = None):
+# 	'''This function creates a simple reaction SMARTS rule based on the
+# 	created and destroyed bonds in the molcule'''
+# 	# Join components separately
 
 
-	# Look until reaction can be initialized
-	while True:
+# 	# Look until reaction can be initialized
+# 	while True:
 
-		# Identify which atom indeces show up multiple times
-		destroyed_bonds_atoms = [[] for x in destroyed_bonds]
-		mapped_atoms = []
-		for i, destroyed_bond in enumerate(destroyed_bonds):
-			atom_indeces = re.findall(r'\:([0-9]*)\]', destroyed_bond)
-			destroyed_bonds_atoms[i] += [int(x) for x in set(atom_indeces)]
-			mapped_atoms += [int(x) for x in set(atom_indeces)]
+# 		# Identify which atom indeces show up multiple times
+# 		destroyed_bonds_atoms = [[] for x in destroyed_bonds]
+# 		mapped_atoms = []
+# 		for i, destroyed_bond in enumerate(destroyed_bonds):
+# 			atom_indeces = re.findall(r'\:([0-9]*)\]', destroyed_bond)
+# 			destroyed_bonds_atoms[i] += [int(x) for x in set(atom_indeces)]
+# 			mapped_atoms += [int(x) for x in set(atom_indeces)]
 
-		# Get duplicates
-		duplicates = set([x for x in mapped_atoms if mapped_atoms.count(x) > 1])
-		print('duplicates: {}'.format(duplicates))
+# 		# Get duplicates
+# 		duplicates = set([x for x in mapped_atoms if mapped_atoms.count(x) > 1])
+# 		print('duplicates: {}'.format(duplicates))
 
-		if not duplicates:
-			break
+# 		if not duplicates:
+# 			break
 
-		# Get the first conflict
-		atom_index = duplicates.pop()
-		print('first duplicate: {}'.format(atom_index))
-		group = '('
-		bonds_in_group = []
-		for i, atom_indeces in enumerate(destroyed_bonds_atoms):
-			if atom_index in atom_indeces:
-				group += destroyed_bonds[i] + '.'
-				bonds_in_group.append(i)
-		group = group[:-1] + ')'
+# 		# Get the first conflict
+# 		atom_index = duplicates.pop()
+# 		print('first duplicate: {}'.format(atom_index))
+# 		group = '('
+# 		bonds_in_group = []
+# 		for i, atom_indeces in enumerate(destroyed_bonds_atoms):
+# 			if atom_index in atom_indeces:
+# 				group += destroyed_bonds[i] + '.'
+# 				bonds_in_group.append(i)
+# 		group = group[:-1] + ')'
 
-		# Update destroyed_bonds to reflect grouping
-		destroyed_bonds[bonds_in_group[0]] = group
-		for bond_index in bonds_in_group[1:]:
-			destroyed_bonds.remove(destroyed_bonds[bond_index])
+# 		# Update destroyed_bonds to reflect grouping
+# 		destroyed_bonds[bonds_in_group[0]] = group
+# 		for bond_index in bonds_in_group[1:]:
+# 			destroyed_bonds.remove(destroyed_bonds[bond_index])
 		
-	# Combine now
-	reactants = '.'.join(destroyed_bonds)
-	products  = '.'.join(created_bonds)
-	if agents:
-		agents = '.'.join(agents)
-	else:
-		agents = ''
+# 	# Combine now
+# 	reactants = '.'.join(destroyed_bonds)
+# 	products  = '.'.join(created_bonds)
+# 	if agents:
+# 		agents = '.'.join(agents)
+# 	else:
+# 		agents = ''
 
-	# Merge 
-	return '{}>{}>{}'.format(reactants, agents, products)
+# 	# Merge 
+# 	return '{}>{}>{}'.format(reactants, agents, products)
 
 def map_reaction(reactants, products):
 	'''Re-assigns atom numbers for reactants and products to reflect most likely
@@ -269,22 +270,36 @@ def get_fragments_for_changed_atoms(mols, changed_atom_tags, radius = 0):
 		fragments += '(' + AllChem.MolFragmentToSmiles(mol, atoms_to_use) + ').'
 	return fragments[:-1]
 
-def main(db_fpath, N = 15):
+def mol_list_to_inchi(mols):
+	'''List of RDKit molecules to InChI string separated by ++'''
+	inchis = [Chem.MolToInchi(mol) for mol in mols]
+	return ' ++ '.join(sorted(inchis))
+
+def mol_list_from_inchi(inchis):
+	'''InChI string separated by ++ to list of RDKit molecules'''
+	return [Chem.MolFromInchi(inchi.strip()) for inchi in inchis.split('++')]
+
+def main(db_fpath, N = 15, radius = 1):
 	'''Read reactions from Lowe's patent reaction SMILES'''
 
 	# Open file
 	data_fid = open(db_fpath, 'r')
 
+	# Define scoring variables
+	total_templates = 0 # total reactions simulated (excludes skipped)
+	total_correct = 0 # actual products predicted
+	total_precise = 0 # ONLY actual products predicted
+
 	# Look for entries
 	for i, line in enumerate(data_fid):
-
-		print('##################################')
-		print('###        RXN {}'.format(i))
-		print('##################################')
 
 		# Are we done?
 		if i == N:
 			break
+
+		print('##################################')
+		print('###        RXN {}'.format(i))
+		print('##################################')
 
 		# Unpack
 		reaction_smiles = line.split('\t')[0].split(' ')[0] # remove fragment portion, too
@@ -302,10 +317,10 @@ def main(db_fpath, N = 15):
 		if err: print('skipping'); continue
 
 		# Draw
-		draw_reaction_smiles(reaction_smiles, fpath = 'test/transforms/rxn_{}.png'.format(i))
+		print(reaction_smiles)
+		#draw_reaction_smiles(reaction_smiles, fpath = 'test/transforms/rxn_{}.png'.format(i))
 
 		# Get fragments
-		radius = 1
 		print('Using radius {} to build fragments'.format(radius))
 		reactant_fragments = get_fragments_for_changed_atoms(reactants, changed_atom_tags, radius = radius)
 		product_fragments  = get_fragments_for_changed_atoms(products,  changed_atom_tags, radius = radius)
@@ -314,50 +329,67 @@ def main(db_fpath, N = 15):
 		rxn_string = '{}>>{}'.format(reactant_fragments, product_fragments)
 		print('Overall fragment transform: {}'.format(rxn_string))
 
-		# # Get bonds
-		# reactant_bonds = all_bonds_in_mol_list(reactants.split('.'))
-		# product_bonds  = all_bonds_in_mol_list(products.split('.'))
+		# Load into RDKit
+		rxn = AllChem.ReactionFromSmarts(rxn_string)
 
-		# # Find how reaction changed bonds
-		# (created_bonds, destroyed_bonds) = \
-		# 	compare_bond_lists(reactant_bonds, product_bonds)
+		# Analyze
+		print('Original number of reactants: {}'.format(len(reactants)))
+		print('Number of reactants in transform: {}'.format(rxn.GetNumReactantTemplates()))
 
-		# # Print
-		# print('reaction {}'.format(i))
-		# print('  reaction:  {}'.format(reaction_smiles))
-		# print('  created:   {}'.format(created_bonds))
-		# if (len(destroyed_bonds) - len(created_bonds)) > 3:
-		# 	print('  destroyed: ** missing product, many bonds lost')
-		# 	continue
-		# else:
-		# 	print('  destroyed: {}'.format(destroyed_bonds))
+		# Run reaction
+		try:
+			# Try all combinations of reactants that fit template
+			combinations = itertools.combinations(reactants, rxn.GetNumReactantTemplates())
+			unique_product_sets = []
+			for combination in combinations:
+				outcomes = rxn.RunReactants(list(combination))
+				if not outcomes: continue
+				print('\nFor reactants {}'.format([Chem.MolToSmiles(mol) for mol in combination]))
+				for j, outcome in enumerate(outcomes):
+					print('- outcome {}/{}'.format(j + 1, len(outcomes)))
+					for k, product in enumerate(outcome):
+						print('  product {}: {}'.format(k, Chem.MolToSmiles(product)))
+					product_set = mol_list_to_inchi(outcome)
+					if product_set not in unique_product_sets:
+						unique_product_sets.append(product_set)
 
-		# # Convert to reaction SMARTS (without agent for now)
-		# rxn_smarts = bond_comparison_to_smarts(created_bonds, destroyed_bonds)
+			print('\nExpctd {}'.format(mol_list_to_inchi(products)))
+			for product_set in unique_product_sets:
+				print('Found  {}'.format(product_set))
 
-		# # Load into rdkit
-		# rxn = AllChem.ReactionFromSmarts(rxn_string)
-		# AllChem.ChemicalReaction.Initialize(rxn)
-		# if not (AllChem.ChemicalReaction.IsInitialized(rxn)):
-		# 	print('Could not intiialize reaction: {}'.format(rxn_smarts))
+			if mol_list_to_inchi(products) in unique_product_sets:
+				print('\nSuccessfully found true products!')
+				total_correct += 1
+				if len(unique_product_sets) > 1:
+					print('...but also found {} more'.format(len(unique_product_sets) - 1))
+				else:
+					total_precise += 1
+			else:
+				print('\nDid not find true products')
+				if len(unique_product_sets) > 1:
+					print('...but found {} unexpected ones'.format(len(unique_product_sets)))
 			
-
-		# # Test reaction
-		# print('reaction test')
-		# outcomes = rxn.RunReactants(reactants)
-		# for j, outcome in outcomes:
-		# 	print('- outcome {}/{}'.format(j, len(outcomes)))
-		# 	for k, product in outcome:
-		# 		print('  product {}: {}'.format(k, Chem.MolToSmiles(product)))
+			total_templates += 1
 		
+		except Exception as e:
+			print(e)
+			print('skipping')
+			continue
+
+
 		# Report progress
 		if (i % 1000) == 0:
 			print('{}/{}'.format(i, N))
 
 		# Pause
-		raw_input('Enter anything to continue...')
+		# raw_input('Enter anything to continue...')
 
-	print('...finished looking through data')
+	print('...finished looking through N reaction records')
+
+	print('Correct product predictions in {}/{} ({}%) cases'.format(total_correct, 
+		total_templates, total_correct * 100.0 / total_templates))
+	print('Specific product predictions in {}/{} ({}%) cases'.format(total_precise, 
+		total_templates, total_precise * 100.0 / total_templates))
 
 	return True
 
@@ -368,7 +400,9 @@ if __name__ == '__main__':
 		quit(1)
 
 	# Run 
-	if len(sys.argv) == 3:
+	if len(sys.argv) >= 4:
+		main(sys.argv[1], N = int(sys.argv[2]), radius = int(sys.argv[3]))
+	elif len(sys.argv) >= 3:
 		main(sys.argv[1], N = int(sys.argv[2]))
 	else:
 		main(sys.argv[1])
