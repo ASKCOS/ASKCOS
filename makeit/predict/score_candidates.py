2a49ad84cb47368cb2a8ca3543ee687efe70badf
connorcoley <connor.coley@gmail.com>
2016-07-12 08:13:17 -0700

    Left out N_h1 as a command-line arg...

--- makeit/predict/score_candidates.py
+++ makeit/predict/score_candidates.py
@@ -192,6 +192,36 @@ def pred_histogram(model, x_files, y_files, z_files, tag = '', split_ratio = 0.8
 	histogram(train_preds, 'TRAIN', 'training{}.png'.format(tag), train_acc)
 	histogram(val_preds, 'VAL', 'validation{}.png'.format(tag), val_acc)
 
+def visualize_weights(model, tag):
+	'''Given a trained model, this function visualizes the weights in each Dense layer'''
+	
+	def vec_to_png(vec, label, title):
+		try:
+			print(vec.shape)
+			fig = plt.figure(figsize=(4,4))
+			plt.pcolor(vec)#, vmin = 0, vmax = 1, cmap = plt.get_cmap('Greens'))
+			plt.title('{} {}'.format(title, vec.shape))
+			cbar = plt.colorbar()
+			#plt.gca().yaxis.set_visible(False)
+			#plt.gca().xaxis.set_visible(False)
+			plt.xlim([0, vec.shape[1]])
+			plt.ylim([0, vec.shape[0]])
+			#plt.subplots_adjust(left = 0, right = 1, top = 0.4, bottom = 0)
+			plt.savefig(os.path.join(FROOT, label) + '.png', bbox_inches = 'tight')
+			plt.close(fig)
+			plt.clf()
+		except:
+			pass
+
+	for i, layer in enumerate(model.layers):
+		try:
+			weights = layer.layer.W.eval()
+			vec_to_png(weights, 'weights{}(layer{})'.format(tag, i), 'Layer {}'.format(i))
+		except Exception as e:
+			print('Skipping layer {}'.format(i))
+			print(e)
+
+
 if __name__ == '__main__':
 
 	FROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'output')
@@ -221,6 +251,8 @@ if __name__ == '__main__':
 						help = 'Learning rate, default 0.0003')
 	parser.add_argument('--dr', type = float, default = 0.5,
 						help = 'Dropout rate, default 0.5')
+	parser.add_argument('--visualize', type = bool, default = False,
+		                help = 'Whether or not to visualize weights ONLY, default False')
 	args = parser.parse_args()
 
 	x_files = sorted([os.path.join(args.data, dfile) \
@@ -254,17 +286,20 @@ if __name__ == '__main__':
 	# print('Mean number of true predictions for each:   {}'.format(np.mean(np.sum(y, axis = 1))))
 
 	if bool(args.retrain):
-		model = model_from_json(open('model{}.json'.format(tag)).read())
+		model = model_from_json(open(os.path.join(FROOT, 'model{}.json'.format(tag))).read())
 		model.compile(loss = 'categorical_crossentropy', 
 			optimizer = SGD(lr = 0.0003, decay = 1e-4, momentum = 0.9),
 			metrics = ['accuracy']
 		)
 		model.load_weights(os.path.join(FROOT, 'weights{}.h5'.format(tag)))
 	else:
-		model = build(N_c = N_c, N_h2 = N_h2, N_h3 = N_h3, l2v = l2v, lr = lr, dr = dr)
+		model = build(N_c = N_c, N_h1 = N_h1, N_h2 = N_h2, N_h3 = N_h3, l2v = l2v, lr = lr, dr = dr)
 	
 	if bool(args.test):
 		pred_histogram(model, x_files, y_files, z_files, tag = tag, split_ratio = 0.8)
+		quit(1)
+	elif bool(args.visualize):
+		visualize_weights(model, tag)
 		quit(1)
 
 	hist = train(model, x_files, y_files, tag = tag, split_ratio = 0.8)
