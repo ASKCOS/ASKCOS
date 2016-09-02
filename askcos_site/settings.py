42152e2aa09efc4bdd96fbb732aea9ad78ce2f86
Connor Coley <ccoley@mit.edu>
2016-09-02 13:28:44 -0400

    Need absolute path for DB

--- askcos_site/settings.py
+++ askcos_site/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'askcos_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 # AUTHENTICATION_BACKENDS = (
