e6c139eb1c837727eccf7ee3b25a3524528b5210
Connor Coley <ccoley@mit.edu>
2017-05-05 13:27:22 -0400

    Maximum price when loading pricer

--- makeit/retro/pricer.py
+++ makeit/retro/pricer.py
@@ -7,67 +7,69 @@ from collections import defaultdict
 from tqdm import tqdm
 
 class Pricer:
-	'''
-	The Pricer class is used to look up the ppg of chemicals if they
-	are buyable.
-	'''
+    '''
+    The Pricer class is used to look up the ppg of chemicals if they
+    are buyable.
+    '''
 
-	def __init__(self, by_xrn = False):
+    def __init__(self, by_xrn = False):
 
-		self.chemicals_source = None
-		self.buyables_source = None
-		self.by_xrn = False
+        self.chemicals_source = None
+        self.buyables_source = None
+        self.by_xrn = False
 
-		self.prices = defaultdict(float) # default 0 ppg means not buyable
-		self.prices_by_xrn = defaultdict(float)
+        self.prices = defaultdict(float) # default 0 ppg means not buyable
+        self.prices_by_xrn = defaultdict(float)
 
-	def load(self, chemicals_collection, buyables_collection):
-		'''
-		Loads the object from a MongoDB collection containing transform
-		template records.
-		'''
-		# Save collection source
-		self.chemicals_source = chemicals_collection
-		self.buyables_source = buyables_collection
+    def load(self, chemicals_collection, buyables_collection, max_ppg=1e10):
+        '''
+        Loads the object from a MongoDB collection containing transform
+        template records.
+        '''
+        # Save collection source
+        self.chemicals_source = chemicals_collection
+        self.buyables_source = buyables_collection
 
 
-		buyable_dict = {}
-		# First pull buyables source (smaller)
-		for buyable_doc in tqdm(self.buyables_source.find({}, ['ppg', 'smiles', 'smiles_flat'], no_cursor_timeout = True)):
-			if USE_STEREOCHEMISTRY:
-				smiles = buyable_doc['smiles']
-			else:
-				smiles = buyable_doc['smiles_flat']
-			buyable_dict[buyable_doc['_id']] = buyable_doc['ppg']
-			if self.prices[smiles]: # already in dict as non-zero, so keep cheaper
-				self.prices[smiles] = min(buyable_doc['ppg'], self.prices[smiles])
-			else:
-				self.prices[smiles] = buyable_doc['ppg']
+        buyable_dict = {}
+        # First pull buyables source (smaller)
+        for buyable_doc in tqdm(self.buyables_source.find({}, ['ppg', 'smiles', 'smiles_flat'], no_cursor_timeout = True)):
+            if USE_STEREOCHEMISTRY:
+                smiles = buyable_doc['smiles']
+            else:
+                smiles = buyable_doc['smiles_flat']
+            if buyable_doc['ppg'] > max_ppg:
+                continue
+            buyable_dict[buyable_doc['_id']] = buyable_doc['ppg']
+            if self.prices[smiles]: # already in dict as non-zero, so keep cheaper
+                self.prices[smiles] = min(buyable_doc['ppg'], self.prices[smiles])
+            else:
+                self.prices[smiles] = buyable_doc['ppg']
 
-		if self.by_xrn:
-			# Then pull chemicals source for XRNs (larger)
-			for chemical_doc in tqdm(self.chemicals_source.find({'buyable_id': {'$gt': -1}}, ['buyable_id'], no_cursor_timeout = True)):
-				if 'buyable_id' not in chemical_doc: continue
-				self.prices_by_xrn[chemical_doc['_id']] = buyable_dict[chemical_doc['buyable_id']]
+        if self.by_xrn:
+            # Then pull chemicals source for XRNs (larger)
+            for chemical_doc in tqdm(self.chemicals_source.find({'buyable_id': {'$gt': -1}}, ['buyable_id'], no_cursor_timeout = True)):
+                if 'buyable_id' not in chemical_doc: continue
+                self.prices_by_xrn[chemical_doc['_id']] = buyable_dict[chemical_doc['buyable_id']]
 
-	def lookup_smiles(self, smiles, alreadyCanonical = False):
-		'''
-		Looks up a price by SMILES. Tries it as-entered and then 
-		re-canonicalizes it in RDKit unl ess the user specifies that
-		the string is definitely already canonical.
-		'''
-		ppg = self.prices[smiles]
-		if not alreadyCanonical:
-			if not ppg:
-				mol = Chem.MolFromSmiles(smiles)
-				if not mol: return ppg
-				smiles = Chem.MolToSmiles(mol, isomericSmiles = USE_STEREOCHEMISTRY)
-				ppg = self.prices[smiles]
-		return ppg
+    def lookup_smiles(self, smiles, alreadyCanonical = False):
+        '''
+        Looks up a price by SMILES. Tries it as-entered and then 
+        re-canonicalizes it in RDKit unl ess the user specifies that
+        the string is definitely already canonical.
+        '''
+        ppg = self.prices[smiles]
+        if not alreadyCanonical:
+            if not ppg:
+                mol = Chem.MolFromSmiles(smiles)
+                if not mol: return ppg
+                smiles = Chem.MolToSmiles(mol, isomericSmiles = USE_STEREOCHEMISTRY)
+                ppg = self.prices[smiles]
+        return ppg
 
-	def lookup_xrn(self, xrn):
-		'''
-		Looks up a price by Reaxys XRN.
-		'''
-		if not self.by_rxn: raise ValueError('Not initialized to look up prices by XRN!')
-		return self.prices_by_xrn[xrn]
+    def lookup_xrn(self, xrn):
+        '''
+        Looks up a price by Reaxys XRN.
+        '''
+        if not self.by_rxn: raise ValueError('Not initialized to look up prices by XRN!')
+        return self.prices_by_xrn[xrn]
