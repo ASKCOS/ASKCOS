b9819e82c36f88cad7b47eb1b35dd9e617a829bf
Connor W. Coley <connor.coley@gmail.com>
2017-05-11 12:11:50 -0400

    Better naming and logging of host workers

--- spawn_workers.sh
+++ spawn_workers.sh
@@ -1,13 +1,7 @@
 source activate askcos
-celery -A askcos_site worker -c 4 -Q tb_worker -n "treebuilder_worker" --max-tasks-per-child 10000 &
-celery -A askcos_site worker -c 1 -Q tb_coordinator -n "treebuilder_coordinator" &
-celery -A askcos_site worker -c 4 -Q fp_worker -n "forwardpredictor_worker" --max-tasks-per-child 10000 &
-celery -A askcos_site worker -c 1 -Q fp_coordinator -n "forwardpredictor_coordinator" &
-celery -A askcos_site worker -c 1 -Q context_worker -n "context_rec" --max-tasks-per-child 50000 &
+celery -A askcos_site worker -c 4 -Q tb_worker -n "treebuilder_worker@$(hostname)" --max-tasks-per-child 10000 --loglevel=INFO --logfile=celery_logs/%p.log &
+celery -A askcos_site worker -c 1 -Q tb_coordinator -n "treebuilder_coordinator@$(hostname)"  --logfile=celery_logs/%p.log &
+celery -A askcos_site worker -c 4 -Q fp_worker -n "forwardpredictor_worker@$(hostname)" --max-tasks-per-child 10000 --loglevel=INFO  --logfile=%p.log &
+celery -A askcos_site worker -c 1 -Q fp_coordinator -n "forwardpredictor_coordinator@$(hostname)" --loglevel=INFO  --logfile=%p.log &
+celery -A askcos_site worker -c 1 -Q context_worker -n "context_rec@$(hostname)" --max-tasks-per-child 50000 --loglevel=INFO  --logfile=%p.log &
 source deactivate
-
-
-
-# source activate askcos
-# celery -A askcos_site multi start tb_coordinator@%h fp_coordinator@%h context_worker@%h tb_worker@%h fp_worker@%h -c:1-3 1 -c:4-5 4 -Q:1 tb_coordinatotr -Q:2 fp_coordinator -Q:3 context_worker -Q:4 tb_worker -Q:5 fp_worker
-# source deactivate
