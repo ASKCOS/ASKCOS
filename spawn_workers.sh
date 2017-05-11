f2638c59e098dca24215b9ba0797fd455a3c293d
Connor W. Coley <connor.coley@gmail.com>
2017-05-11 19:08:12 -0400

    New spawn workers: multiple tree_builder groups that are reservable

--- spawn_workers.sh
+++ spawn_workers.sh
@@ -1,7 +1,19 @@
 source activate askcos
-celery -A askcos_site worker -c 4 -Q tb_worker -n "treebuilder_worker@$(hostname)" --max-tasks-per-child 10000 --loglevel=INFO --logfile=celery_logs/%p.log &
-celery -A askcos_site worker -c 1 -Q tb_coordinator -n "treebuilder_coordinator@$(hostname)"  --logfile=celery_logs/%p.log &
-celery -A askcos_site worker -c 4 -Q fp_worker -n "forwardpredictor_worker@$(hostname)" --max-tasks-per-child 10000 --loglevel=INFO  --logfile=%p.log &
-celery -A askcos_site worker -c 1 -Q fp_coordinator -n "forwardpredictor_coordinator@$(hostname)" --loglevel=INFO  --logfile=%p.log &
-celery -A askcos_site worker -c 1 -Q context_worker -n "context_rec@$(hostname)" --max-tasks-per-child 50000 --loglevel=INFO  --logfile=%p.log &
+export CELERY_LOG_LEVEL="WARN"
+
+# Purge reservable queues to avoid weird conflicts
+celery amqp queue.purge tb_worker_reservable
+
+# The core workers 
+# note: tb_worker is avilable for one-step even if tb_coordinator reserves all orders
+celery -A askcos_site worker -c 2 -Q tb_worker -n "treebuilder_worker@$(hostname)" --max-tasks-per-child 10000 --loglevel=$CELERY_LOG_LEVEL --logfile=celery_logs/%p.log &
+celery -A askcos_site worker -c 3 -Q tb_coordinator -n "treebuilder_coordinator@$(hostname)"  --logfile=celery_logs/%p.log &
+celery -A askcos_site worker -c 8 -Q fp_worker -n "forwardpredictor_worker@$(hostname)" --max-tasks-per-child 10000 --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p.log &
+celery -A askcos_site worker -c 2 -Q fp_coordinator -n "forwardpredictor_coordinator@$(hostname)" --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p.log &
+celery -A askcos_site worker -c 2 -Q context_worker -n "context_rec@$(hostname)" --max-tasks-per-child 50000 --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p.log &
+
+# Some bonus worker pools for the treebuilder(s) to reserve
+celery -A askcos_site worker -c 4 -Q tb_worker,tb_worker_reservable -n "treebuilder_worker_pool1@$(hostname)" --max-tasks-per-child 10000 --loglevel=$CELERY_LOG_LEVEL --logfile=celery_logs/%p.log &
+celery -A askcos_site worker -c 4 -Q tb_worker,tb_worker_reservable -n "treebuilder_worker_pool2@$(hostname)" --max-tasks-per-child 10000 --loglevel=$CELERY_LOG_LEVEL --logfile=celery_logs/%p.log &
+celery -A askcos_site worker -c 4 -Q tb_worker,tb_worker_reservable -n "treebuilder_worker_pool3@$(hostname)" --max-tasks-per-child 10000 --loglevel=$CELERY_LOG_LEVEL --logfile=celery_logs/%p.log &
 source deactivate
