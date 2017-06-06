fbfd644ff78d710646d1879750509e2715866910
Connor W. Coley <connor.coley@gmail.com>
2017-06-06 12:21:30 -0400

    global dadtabase, shared b/w users

--- askcos_site/settings.py
+++ askcos_site/settings.py
@@ -90,7 +90,8 @@ WSGI_APPLICATION = 'askcos_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/data', 'www-data', 'db.sqlite3'),
     }
 }
 # AUTHENTICATION_BACKENDS = (
