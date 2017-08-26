27d4011c58f9383d4c699ec8b5d813f5d8696f27
Connor W. Coley <connor.coley@gmail.com>
2017-08-26 16:44:02 -0400

    Updated index to have more helpful information

--- askcos_site/settings.py
+++ askcos_site/settings.py
@@ -27,7 +27,7 @@ from askcos_celery.celeryconfig import *
 SECRET_KEY = 'px$*ir)-wd=x6^!r++t53ik^2)z7!9cvw+m#@!-$ut@xjyjtg*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['askcos.mit.edu']
 
@@ -163,7 +163,7 @@ RETRO_TRANSFORMER = {
 SYNTH_TRANSFORMS = {
     'database': 'reaxys',
     'collection': 'transforms_forward_v1',
-    'mincount': 25, 
+    'mincount': 5, 
 }
 SYNTH_TRANSFORMER = {
 }
