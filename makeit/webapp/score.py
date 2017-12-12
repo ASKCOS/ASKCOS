b0c5b178adca7f1c2e5d93bd182982546c07abba
Connor W. Coley <connor.coley@gmail.com>
2017-12-12 15:34:51 -0500

    New scoring function using SCSCore in score.py

--- makeit/webapp/score.py
+++ makeit/webapp/score.py
@@ -1,6 +1,9 @@
 import numpy as np
 import rdkit.Chem as Chem
 
+from scscore.standalone_model_numpy import SCScorer 
+scscorer = SCScorer()
+scscorer.restore()
 
 def score_smiles(smiles, Pricer=None):
     if Pricer is not None:
@@ -14,3 +17,16 @@ def score_smiles(smiles, Pricer=None):
     chiral_centers = len(Chem.FindMolChiralCenters(x))
 
     return - 2.00 * np.power(total_atoms, 1.5) - 1.00 * np.power(ring_bonds, 1.5) - 2.00 * np.power(chiral_centers, 2.0)
+
+def score_smiles_scscore(smiles, Pricer=None):
+    score = 0.
+    for smi in smiles.split('.'):
+        if Pricer is not None:
+            ppg = Pricer.lookup_smiles(smiles, alreadyCanonical=True)
+            if ppg:
+                score -= ppg # basically "free"
+                continue
+
+        (smi, sco) = scscorer.get_score_from_smi(smi)
+        score -= np.power(8., sco)
+    return score
