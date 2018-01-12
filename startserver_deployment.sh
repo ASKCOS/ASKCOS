02b19bff93f9acafa2cfb94fe0ff204d26448808
Connor Coley <connor.coley@gmail.com>
2018-01-11 20:10:44 -0500

    Removing garbage scripts

--- startserver_deployment.sh
+++
@@ -1,8 +0,0 @@
-# Deploy website - first copy correct settings over
-cp askcos_site/settings_deployment.py askcos_site/settings.py
-if ! pidof apache2 > /dev/null
-then
-    python manage.py runserver
-else
-    sudo service apache2 reload
-fi
