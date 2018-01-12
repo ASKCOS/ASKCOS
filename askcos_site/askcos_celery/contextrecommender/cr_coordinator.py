5673fbde747db91e5e76b8a131c6b40bb7de6718
Connor Coley <connor.coley@gmail.com>
2018-01-11 20:17:48 -0500

    Cleaning up askcos_celery modules - No more fixed database usage - PEP8

--- askcos_site/askcos_celery/contextrecommender/cr_coordinator.py
+++ askcos_site/askcos_celery/contextrecommender/cr_coordinator.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals, print_function
 from celery import shared_task
 from celery.signals import celeryd_init
-from celery.result import allow_join_result 
+from celery.result import allow_join_result
 from makeit.synthetic.context.nn_context_recommender import NNContextRecommender
 from askcos_site.askcos_celery.contextrecommender.cr_nn_worker import get_n_conditions
 import makeit.global_config as gc
@@ -10,29 +10,29 @@ import time
 
 @celeryd_init.connect
 def configure_worker(options={}, **kwargs):
-    if 'queues' not in options: 
-        return 
+    if 'queues' not in options:
+        return
     if CORRESPONDING_QUEUE not in options['queues'].split(','):
         return
     print('### STARTING UP A CONTEXT RECOMMENDER COORDINATOR ###')
-
     print('### CONTEXT RECOMMENDER COORDINATOR STARTED UP ###')
 
 
 @shared_task
-def get_context_recommendations(rxn, n=10, singleSlvt=True, with_smiles=True, context_recommender = ''):
+def get_context_recommendations(rxn, n=10, singleSlvt=True, with_smiles=True, context_recommender=''):
     '''Retrieve a context recommendation given the reaction to attempt.
 
     rxn = [reacants, products], where each is a list of SMILES
     n = number of contexts to return'''
 
     global NN_PREDICTOR
-    
+
     print('Context context_recommender worker got a request for rxn {} and n {}'.format(
         rxn, n))
     with allow_join_result():
         if context_recommender == gc.nearest_neighbor:
-            res = get_n_conditions.delay(rxn, n=10, singleSlvt=True, with_smiles=True)
+            res = get_n_conditions.delay(
+                rxn, n=10, singleSlvt=True, with_smiles=True)
             return res.get()
         else:
             raise NotImplementedError
--- askcos_site/askcos_celery/contextrecommender/cr_nn_worker.py
+++ askcos_site/askcos_celery/contextrecommender/cr_nn_worker.py
@@ -6,6 +6,7 @@ must query the database to get details about the instance.
 '''
 
 from __future__ import absolute_import, unicode_literals, print_function
+from django.conf import settings
 from celery import shared_task
 from celery.signals import celeryd_init
 from makeit.synthetic.context.nn_context_recommender import NNContextRecommender
@@ -13,21 +14,16 @@ import makeit.global_config as gc
 
 CORRESPONDING_QUEUE = 'cr_nn_worker'
 
+
 @celeryd_init.connect
 def configure_worker(options={}, **kwargs):
-    if 'queues' not in options: 
-        return 
+    if 'queues' not in options:
+        return
     if CORRESPONDING_QUEUE not in options['queues'].split(','):
         return
     print('### STARTING UP A NEAREST NEIGHBOR CONTEXT RECOMMENDER WORKER ###')
 
     global recommender
-
-    # Get Django settings
-    from django.conf import settings
-
-    # Database
-    from database import db_client
 
     # Setting logging low
     from rdkit import RDLogger
@@ -35,7 +31,8 @@ def configure_worker(options={}, **kwargs):
     lg.setLevel(RDLogger.CRITICAL)
     try:
         recommender = NNContextRecommender()
-        recommender.load_nn_model(model_path = gc.CONTEXT_REC['model_path'], info_path = gc.CONTEXT_REC['info_path'])
+        recommender.load_nn_model(model_path=gc.CONTEXT_REC[
+                                  'model_path'], info_path=gc.CONTEXT_REC['info_path'])
     except Exception as e:
         print(e)
     print('Loaded context recommendation model')
@@ -55,6 +52,7 @@ def get_n_conditions(rxn, n=10, singleSlvt=True, with_smiles=True):
     print('Context recommender worker got a request for rxn {} and n {}'.format(
         rxn, n))
 
-    res =  recommender.get_n_conditions(rxn, n=n, singleSlvt=singleSlvt, with_smiles=with_smiles)
+    res = recommender.get_n_conditions(
+        rxn, n=n, singleSlvt=singleSlvt, with_smiles=with_smiles)
     print('Task completed, returning results.')
     return res
