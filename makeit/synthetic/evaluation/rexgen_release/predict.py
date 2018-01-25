2fecbadc94a494aea94db0323cb66714bc962854
Connor W. Coley <connor.coley@gmail.com>
2018-01-25 16:44:03 -0500

    Error handling in template-free prediction

--- makeit/synthetic/evaluation/rexgen_release/predict.py
+++ makeit/synthetic/evaluation/rexgen_release/predict.py
@@ -14,10 +14,13 @@ class TFFP():
         self.ranker = CandRanker(hidden_size=320, depth=3, TOPK=100)
         self.ranker.load_model(os.path.join(froot, 'CandRanker', 'uspto-320-3'))
 
-    def predict(self, smi, top_n=25, num_core=8):
+    def predict(self, smi, top_n=100, num_core=8):
         m = Chem.MolFromSmiles(smi)
         if not m:
-            raise ValueError('Could not parse molecule for TFFP! {}'.format(smi))
+            if smi[-1] == '.':
+                m = Chem.MolFromSmiles(smi[:-1])
+            if not m:
+                raise ValueError('Could not parse molecule for TFFP! {}'.format(smi))
         [a.SetIntProp('molAtomMapNumber', i+1) for (i, a) in enumerate(m.GetAtoms())]
         s = Chem.MolToSmiles(m)
         rcores = self.finder.predict([s], num_core=num_core)[0]
