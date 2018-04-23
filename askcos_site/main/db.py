cea62940ae4df6cb8af3f705db21b51f430fbb26
Connor W. Coley <connor.coley@gmail.com>
2018-04-23 11:49:31 -0400

    Updated synth default to use forward predictor - Now include reagents/solvent in enumeration - New DB link too

--- askcos_site/main/db.py
+++ askcos_site/main/db.py
@@ -4,5 +4,5 @@ global db_client
 try:
 	db_client
 except NameError:
-	db_client = MongoClient('mongodb://guest:guest@rmg.mit.edu/admin', 27017)
+	db_client = MongoClient('mongodb://guest:guest@askcos2.mit.edu/admin', 27017)
 	#print(db_client.database_names())
