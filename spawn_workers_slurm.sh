083c06f67fe454ff09c290a8d90eefe62d8fc0ff
Connor W. Coley <connor.coley@gmail.com>
2018-04-30 19:59:55 -0400

    Random ID for slurm

--- spawn_workers_slurm.sh
+++ spawn_workers_slurm.sh
@@ -11,6 +11,6 @@ source activate askcos
 export CELERY_LOG_LEVEL="WARN"
 
 # Chiral retro transformers
-KERAS_BACKEND=theano THEANO_FLAGS=device=cpu celery -A askcos_site worker -c 12 -Q tb_c_worker,tb_c_worker_reservable -n "tb_c_worker_pool1@$(hostname)" --maxtasksperchild 5000 --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p_$RANDOM.log
+KERAS_BACKEND=theano THEANO_FLAGS=device=cpu celery -A askcos_site worker -c 12 -Q tb_c_worker,tb_c_worker_reservable -n "tb_c_worker_pool$RANDOM@$(hostname)" --maxtasksperchild 5000 --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p_$RANDOM.log
 
 source deactivate
