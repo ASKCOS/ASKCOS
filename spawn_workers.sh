9098ff2503791f493bb18cf88153de1f989c4977
Connor W. Coley <connor.coley@gmail.com>
2018-04-30 20:00:21 -0400

    One more worker to handle fast_fitler requests

--- spawn_workers.sh
+++ spawn_workers.sh
@@ -23,7 +23,7 @@ KERAS_BACKEND=theano THEANO_FLAGS=device=cpu celery -A askcos_site worker -c 6 -
 KERAS_BACKEND=theano THEANO_FLAGS=device=cpu celery -A askcos_site worker -c 8 -Q tb_c_worker,tb_c_worker_reservable -n "tb_c_worker_pool1@$(hostname)" --maxtasksperchild 5000 --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p.log &
 #KERAS_BACKEND=theano THEANO_FLAGS=device=cpu celery -A askcos_site worker -c 8 -Q tb_c_worker,tb_c_worker_reservable -n "tb_c_worker_pool2@$(hostname)" --maxtasksperchild 5000 --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p.log &
 #KERAS_BACKEND=theano THEANO_FLAGS=device=cpu celery -A askcos_site worker -c 8 -Q tb_c_worker,tb_c_worker_reservable -n "tb_c_worker_pool3@$(hostname)" --maxtasksperchild 5000 --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p.log &
-KERAS_BACKEND=theano THEANO_FLAGS=device=cpu celery -A askcos_site worker -c 3 -Q tb_c_worker -n "tb_c_worker@$(hostname)" --maxtasksperchild 1000 --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p.log &
+KERAS_BACKEND=theano THEANO_FLAGS=device=cpu celery -A askcos_site worker -c 4 -Q tb_c_worker -n "tb_c_worker@$(hostname)" --maxtasksperchild 1000 --loglevel=$CELERY_LOG_LEVEL  --logfile=celery_logs/%p.log &
 
 # Tree builder coordinator
 KERAS_BACKEND=theano THEANO_FLAGS=device=cpu celery -A askcos_site worker -c 5 -Q tb_coordinator -n "tb_coordinator@$(hostname)"  --maxtasksperchild 5 --logfile=celery_logs/%p.log &
