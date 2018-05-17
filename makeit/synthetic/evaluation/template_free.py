3f494befa7e2d08e6e07c661a372593a81762eec
Connor W. Coley <connor.coley@gmail.com>
2018-05-17 13:37:29 -0400

    Template free method uses context

--- makeit/synthetic/evaluation/template_free.py
+++ makeit/synthetic/evaluation/template_free.py
@@ -30,68 +30,79 @@ class TemplateFreeNeuralNetScorer(Scorer):
         from makeit.synthetic.evaluation.rexgen_release.predict import TFFP
         self.model = TFFP() 
 
-    def evaluate(self, reactants_smiles, contexts=None, **kwargs):
+    def evaluate(self, reactants_smiles, contexts=[(20,'','','','','')], **kwargs):
         '''Evaluation does not use context, but left as dummy pos var'''
         ##what if context contributes necessary atoms??
-        outcomes = self.model.predict(reactants_smiles, 
-            top_n=kwargs.get('top_n', 1e10), num_core=kwargs.get('num_core', 8))
-        if not outcomes:
-            return [[{
-                'rank': 1,
-                'outcome': {'smiles': '', 'template_ids':[], 'num_examples':0},
-                'score': 0,
-                'prob': 0,
-                }]]
-         
-        outcomes_to_ret = {}
-        reactants_smiles_split = reactants_smiles.split('.')
-        for outcome in outcomes:
 
-            # Remove fragments that are unreacted
-            smiles_list = set(outcome['smiles'].split('.'))
-            for reactants_smiles_part in reactants_smiles_split:
-                if reactants_smiles_part in smiles_list:
-                    smiles_list.remove(reactants_smiles_part)
-            if not smiles_list:
-                continue # no reaction?
-
-            # Canonicalize
-            mol = Chem.MolFromSmiles('.'.join(smiles_list))
-            if not mol:
-                MyLogger.print_and_log('Template free evaluator could not reparse product {}'.format('.'.join(smiles_list)), 
-                    template_free_scorer_loc, 1)
+        all_outcomes = []
+        for (T1, slvt1, rgt1, cat1, t1, y1) in contexts:
+            this_reactants_smiles = reactants_smiles
+            if slvt1:
+                this_reactants_smiles += '.' + slvt1
+            if rgt1:
+                this_reactants_smiles += '.' + rgt1
+            if cat1:
+                this_reactants_smiles += '.' + cat1
+            outcomes = self.model.predict(this_reactants_smiles, 
+                top_n=kwargs.get('top_n', 1e10), num_core=kwargs.get('num_core', 8))
+            if not outcomes:
+                all_outcomes.append([{
+                    'rank': 1,
+                    'outcome': {'smiles': '', 'template_ids':[], 'num_examples':0},
+                    'score': 0,
+                    'prob': 0,
+                    }])
                 continue
-            smiles_list = Chem.MolToSmiles(mol).split('.')
-            smiles = max(smiles_list, key=len) # NOTE: this is not great...byproducts may be longer
+             
+            outcomes_to_ret = {}
+            reactants_smiles_split = this_reactants_smiles.split('.')
+            for outcome in outcomes:
 
-            if smiles in outcomes_to_ret:
-                outcomes_to_ret[smiles]['rank'] = min(outcomes_to_ret[smiles]['rank'], outcome['rank'])
-                outcomes_to_ret[smiles]['score'] = max(outcomes_to_ret[smiles]['score'], outcome['score'])
-                outcomes_to_ret[smiles]['prob'] += outcome['prob']
-            else:
-                # Append outcome information
-                outcomes_to_ret[smiles] = {
-                    'rank': outcome['rank'],
-                    'outcome': {
-                        'smiles': smiles,
-                        # 'smiles_list': smiles_list,
-                        'template_ids': [],
-                        'num_examples': 0,
-                    },
-                    'score': float(outcome['score']),
-                    'prob': float(outcome['prob'])
-                }
+                # Remove fragments that are unreacted
+                smiles_list = set(outcome['smiles'].split('.'))
+                for reactants_smiles_part in reactants_smiles_split:
+                    if reactants_smiles_part in smiles_list:
+                        smiles_list.remove(reactants_smiles_part)
+                if not smiles_list:
+                    continue # no reaction?
 
-        # Renormalize and re-rank
-        outcomes = sorted(outcomes_to_ret.values(), key=lambda x: x['prob'], reverse=True)
-        total_prob = sum([outcome['prob'] for outcome in outcomes])
-        for i, outcome in enumerate(outcomes):
-            outcomes[i]['rank'] = i + 1
-            outcomes[i]['prob'] = outcome['prob'] / total_prob
+                # Canonicalize
+                mol = Chem.MolFromSmiles('.'.join(smiles_list))
+                if not mol:
+                    MyLogger.print_and_log('Template free evaluator could not reparse product {}'.format('.'.join(smiles_list)), 
+                        template_free_scorer_loc, 1)
+                    continue
+                smiles_list = Chem.MolToSmiles(mol).split('.')
+                smiles = max(smiles_list, key=len) # NOTE: this is not great...byproducts may be longer
 
+                if smiles in outcomes_to_ret:
+                    outcomes_to_ret[smiles]['rank'] = min(outcomes_to_ret[smiles]['rank'], outcome['rank'])
+                    outcomes_to_ret[smiles]['score'] = max(outcomes_to_ret[smiles]['score'], outcome['score'])
+                    outcomes_to_ret[smiles]['prob'] += outcome['prob']
+                else:
+                    # Append outcome information
+                    outcomes_to_ret[smiles] = {
+                        'rank': outcome['rank'],
+                        'outcome': {
+                            'smiles': smiles,
+                            # 'smiles_list': smiles_list,
+                            'template_ids': [],
+                            'num_examples': 0,
+                        },
+                        'score': float(outcome['score']),
+                        'prob': float(outcome['prob'])
+                    }
 
+            # Renormalize and re-rank
+            outcomes = sorted(outcomes_to_ret.values(), key=lambda x: x['prob'], reverse=True)
+            total_prob = sum([outcome['prob'] for outcome in outcomes])
+            for i, outcome in enumerate(outcomes):
+                outcomes[i]['rank'] = i + 1
+                outcomes[i]['prob'] = outcome['prob'] / total_prob
+
+            all_outcomes.append(outcomes)
         # Return in lists as if we received a list of contexts
-        return [outcomes]
+        return all_outcomes
                 
 
 if __name__ == '__main__':
