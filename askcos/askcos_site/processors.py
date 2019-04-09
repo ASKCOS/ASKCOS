03eb64ea4234bfca819a4d5ce8ead945c8af0746
Mike Fortunato <mef231@gmail.com>
2019-04-09 11:38:07 -0400

    added switch to enable/disable forgot password link (#121)

--- askcos/askcos_site/processors.py
+++ askcos/askcos_site/processors.py
@@ -3,6 +3,7 @@ import os
 def customization(request):
     customizable = {
         'organization': os.environ.get('ORGANIZATION', 'MIT'),
-        'contact_email': os.environ.get('CONTACT_EMAIL', 'ccoley@mit.edu')
+        'contact_email': os.environ.get('CONTACT_EMAIL', 'ccoley@mit.edu'),
+        'email_enabled': bool(os.environ.get('EMAIL_ENABLED', False))
     }
     return customizable
