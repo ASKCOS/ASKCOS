40713cc23628008469d91c2a5daac84d31a65250
Mike Fortunato <mef231@gmail.com>
2019-04-23 15:42:37 -0400

    adds support popup (#142)
    * addresses #137; adds support popup and formats email subject line with values from support form

---
+++ askcos/askcos_site/static/support.js
@@ -0,0 +1,26 @@
+function showPopup() {
+  document.querySelector('.support-popup').style.display = 'block';
+}
+
+function hidePopup() {
+  document.querySelector('.support-popup').style.display = 'none';
+}
+
+function submitSupport() {
+  var mailtoString = "mailto:mlpds_support@mit.edu"
+  mailtoString += "?subject="+document.querySelector('#support-module').value
+  mailtoString += "|"+document.querySelector('#support-category').value
+  if (document.querySelector('#support-shared').checked) {
+    mailtoString += "|shared"
+  }
+  else {
+    mailtoString += "|"
+  }
+  mailtoString += "| "+document.querySelector('#support-subject').value
+  mailtoString += "&body=Please add your comments here. Please don't modify the beginning of the subject line; we use this for internal purposes"
+  window.location = mailtoString;
+}
+
+document.querySelector('#support-anchor').onclick = showPopup;
+document.querySelector('#support-submit').onclick = submitSupport;
+document.querySelector('#support-cancel').onclick = hidePopup;
