440ab189c9250de79d3817b5f89ad1f43a569349
Mike Fortunato <mef231@gmail.com>
2019-10-01 15:05:32 +0000

    Fix restore of saved results from versions prior to v0.3.0
    Former-commit-id: af05abeb803ec62c021796dfedbd2da8b8c949de

--- deploy/transfer_results_to_mongo.py
+++ deploy/transfer_results_to_mongo.py
@@ -1,7 +1,10 @@
 import sys
 import json
+from pathlib import Path
 from pymongo import MongoClient
 from makeit import global_config as gc
+
+user_save_path = Path(gc.__file__).parent / 'data' / 'user_saves'
 
 if len(sys.argv) == 2:
     db_json = sys.argv[1]
@@ -22,18 +25,22 @@ with open(db_json) as f:
 saved_results = [thing for thing in j if thing['model'] == 'main.savedresults']
 
 for result in saved_results:
-    if not result['fields'].get('fpath'): continue
-    path = result['fields'].pop('fpath')
-    result_id = path.split('/')[-1].split('.')[0]
-    result['fields']['result_id'] = result_id
-    result['fields']['result_type'] = 'html'
-    with open(path) as f:
-        html = f.read()
-    results_collection.update(
-        {'_id': result_id},
-        {'result': html},
-        upsert=True
-    )
+    try:
+        if not result['fields'].get('fpath'): continue
+        path = result['fields'].pop('fpath')
+        result_id = path.split('/')[-1].split('.')[0]
+        result['fields']['result_id'] = result_id
+        result['fields']['result_type'] = 'html'
+        with open(str((user_save_path / result_id).with_suffix('.txt'))) as f:
+            html = f.read()
+        results_collection.update(
+            {'_id': result_id},
+            {'result': html},
+            upsert=True
+        )
+    except Exception as e:
+        print(e)
+        print('failed to restore: ' + path)
 
 with open(db_json, 'w') as f:
     json.dump(j, f)
