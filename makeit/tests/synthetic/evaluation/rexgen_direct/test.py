504097f09b72fa432bce4361dde4368072e8a4e3
Max Liu <mjliu@mit.edu>
2020-01-22 13:23:16 -0500

    Fix rexgen_direct.predict unit test
    Update return value of TFFP.predict
    Use assertAlmostEqual for numerical comparisons
    PEP-8 style fixes

--- makeit/tests/synthetic/evaluation/rexgen_direct/test.py
+++ makeit/tests/synthetic/evaluation/rexgen_direct/test.py
@@ -1,20 +1,30 @@
-import makeit.synthetic.evaluation.rexgen_direct.predict as p
-import unittest
 import os
-import sys
-is_py2 = sys.version[0] == '2'
-if is_py2:
-    import cPickle as pickle
-else:
-    import pickle as pickle
+import pickle
+import unittest
+
+import makeit.synthetic.evaluation.rexgen_direct.predict as p
+
 
 class TestPredict(unittest.TestCase):
+
     def test_01_predict(self):
+        """Test template free forward predictor."""
         tffp = p.TFFP()
-        result = tffp.predict('CCCO.CCCBr')
+        rsmi_am, result = tffp.predict('CCCO.CCCBr')
+
+        self.assertEqual('[CH3:1][CH2:2][CH2:3][OH:4].[CH3:5][CH2:6][CH2:7][Br:8]', rsmi_am)
+
         with open(os.path.join(os.path.dirname(__file__), 'expected/predict.pkl'), 'rb') as t:
-            expected = pickle.load(t)
-        self.assertEqual(expected, result)
+            expected = pickle.load(t, encoding='iso-8859-1')
+
+        self.assertEqual(len(expected), len(result))
+
+        for e, r in zip(expected, result):
+            self.assertEqual(e['smiles'], r['smiles'])
+            self.assertEqual(e['rank'], r['rank'])
+            self.assertAlmostEqual(e['prob'], r['prob'], places=4)
+            self.assertAlmostEqual(e['score'], r['score'], places=4)
+
 
 if __name__ == '__main__':
     res = unittest.main(verbosity=3, exit=False)
