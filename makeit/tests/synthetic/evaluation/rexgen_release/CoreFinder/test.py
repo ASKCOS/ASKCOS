e3d8a673b32d6663cb8c6773534709b115ca50e6
Max Liu <mjliu@mit.edu>
2020-01-22 14:03:59 -0500

    Update rexgen_release unit tests
    Fix encoding when loading pickles
    Use assertAlmostEqual for numerical comparisons
    PEP-8 style fixes

--- makeit/tests/synthetic/evaluation/rexgen_release/CoreFinder/test.py
+++ makeit/tests/synthetic/evaluation/rexgen_release/CoreFinder/test.py
@@ -1,20 +1,21 @@
-import makeit.synthetic.evaluation.rexgen_release.CoreFinder.mol_graph as mg
-import unittest
-import numpy as np
 import os
+import pickle
 import sys
-is_py2 = sys.version[0] == '2'
-if is_py2:
-    import cPickle as pickle
-else:
-    import pickle as pickle
+import unittest
+
+import numpy as np
+
+import makeit.synthetic.evaluation.rexgen_release.CoreFinder.mol_graph as mg
+
 
 class TestCFMolGraph(unittest.TestCase):
+
     def test_01_smiles2graph_batch(self):
+        """Test makeit.synthetic.evaluation.rexgen_release.CoreFinder.mol_graph.smiles2graph_batch"""
         np.set_printoptions(threshold=sys.maxsize)
-        result = mg.smiles2graph_batch(["c1cccnc1",'c1nccc2n1ccc2'])
+        result = mg.smiles2graph_batch(["c1cccnc1", 'c1nccc2n1ccc2'])
         with open(os.path.join(os.path.dirname(__file__), 'expected/CF_smiles2graph_batch.pkl'), 'rb') as t:
-            expected = pickle.load(t)
+            expected = pickle.load(t, encoding='iso-8859-1')
         for e, r in zip(expected, result):
             self.assertTrue((e == r).all())
 
