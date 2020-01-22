9902483b2fdb59c599e921b811cbaeb6b01564af
Max Liu <mjliu@mit.edu>
2020-01-22 14:13:38 -0500

    Update chemhistorian unit test
    Update expected value to match function output
    PEP-8 style fixes

--- makeit/tests/utilities/historian/test.py
+++ makeit/tests/utilities/historian/test.py
@@ -1,15 +1,21 @@
 import unittest
+
 from makeit.utilities.historian.chemicals import ChemHistorian
 
+
 class TestChemHistorian(unittest.TestCase):
+
     def setUp(self):
+        """This method is run once before each test in this class."""
         self.chemhistorian = ChemHistorian(hashed=True)
         self.chemhistorian.load()
 
     def test_01_lookup_smiles(self):
+        """Test that we can look up a SMILES string in chemhistorian."""
         result = self.chemhistorian.lookup_smiles('CCCCO')
-        expected = {'as_reactant_refs': [], 'as_product_refs': [], 'as_reactant': 17450, 'as_product': 2726}
+        expected = {'as_product': 2726, 'as_reactant': 17450, 'template_set': 'reaxys'}
         self.assertEqual(expected, result)
+
 
 if __name__ == '__main__':
     res = unittest.main(verbosity=3, exit=False)
