12b747ac91e8f201579d381e3845fa687abf8bac
Max Liu <mjliu@mit.edu>
2020-01-22 13:29:47 -0500

    Fix rexgen_direct.core_wln_global unit tests
    Fix encoding when loading pickles
    PEP-8 style fixes

--- makeit/tests/synthetic/evaluation/rexgen_direct/core_wln_global/test.py
+++ makeit/tests/synthetic/evaluation/rexgen_direct/core_wln_global/test.py
@@ -1,30 +1,30 @@
+import os
+import pickle
+import sys
+import unittest
+
+import numpy as np
+
 import makeit.synthetic.evaluation.rexgen_direct.core_wln_global.mol_graph as mg
 import makeit.synthetic.evaluation.rexgen_direct.core_wln_global.mol_graph_rich as mgr
-import unittest
-import numpy as np
-import os
-import sys
-is_py2 = sys.version[0] == '2'
-if is_py2:
-    import cPickle as pickle
-else:
-    import pickle as pickle
+
 
 class TestMolGraph(unittest.TestCase):
     def test_01_smiles2graph_list(self):
         np.set_printoptions(threshold=sys.maxsize)
-        result = mg.smiles2graph_list(["c1cccnc1",'c1nccc2n1ccc2'])
+        result = mg.smiles2graph_list(["c1cccnc1", 'c1nccc2n1ccc2'])
         with open(os.path.join(os.path.dirname(__file__), 'expected/mg_smiles2graph_list.pkl'), 'rb') as t:
-            expected = pickle.load(t)
+            expected = pickle.load(t, encoding='iso-8859-1')
         for e, r in zip(expected, result):
             self.assertTrue((e == r).all())
+
 
 class TestMolGraphRich(unittest.TestCase):
     def test_01_smiles2graph_list(self):
         np.set_printoptions(threshold=sys.maxsize)
-        result = mgr.smiles2graph_list(["c1cccnc1",'c1nccc2n1ccc2'])
+        result = mgr.smiles2graph_list(["c1cccnc1", 'c1nccc2n1ccc2'])
         with open(os.path.join(os.path.dirname(__file__), 'expected/mgr_smiles2graph_list.pkl'), 'rb') as t:
-            expected = pickle.load(t)
+            expected = pickle.load(t, encoding='iso-8859-1')
         for e, r in zip(expected, result):
             self.assertTrue((e == r).all())
 
