3127f10f7648aedec8e0e25d41cfeb7b5e7eb213
Max Liu <mjliu@mit.edu>
2020-02-04 13:23:13 -0500

    Fix synthetic.selectivity unit test
    Clear tf sessions to prevent weird errors
    PEP-8 formatting

--- makeit/tests/synthetic/selectivity/test.py
+++ makeit/tests/synthetic/selectivity/test.py
@@ -1,15 +1,26 @@
-from makeit.synthetic.selectivity.site_selectivity import Site_Predictor
-
 import unittest
 
+import tensorflow.compat.v1 as tf
+
+from makeit.synthetic.selectivity.site_selectivity import Site_Predictor
+
+
 class SiteSelectivity(unittest.TestCase):
+
+    def setUp(self):
+        """This method is run once before each test in this class."""
+        # Clear tensorflow sessions - leftover sessions from other models cause unexpected errors
+        tf.keras.backend.clear_session()
+
     def test_01_predict(self):
+        """Test that the Site_Predictor works as expected."""
         react = 'Cc1ccccc1'
         predictor = Site_Predictor()
         res = predictor.predict(react)
         self.assertEqual(len(res), 123)
         self.assertEqual(type(res[0]), dict)
         self.assertEqual(len(res[0].get('atom_scores')), 7)
+
 
 if __name__ == '__main__':
     res = unittest.main(verbosity=3, exit=False)
