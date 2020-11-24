a19a675afd6e45b86fc5652845a9cbb348a56c6e
Max Liu <mjliu@mit.edu>
2020-11-24 13:58:47 -0500

    Fix incorrect module init

--- askcos/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import, unicode_literals, print_function
-
-# This will make sure the app is always importated when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app
-
-__all__ = ['celery_app']
